5a8e769c180e1884417a5c01c69fa063f0ab87f0
Shiva20075 <palleshiva2007@gmail.com>
2026-01-09 22:23:02 +0530

    Fix comment formatting and uncomment query method

--- src/main/java/com/example/JPA/StudentRepo.java
+++ src/main/java/com/example/JPA/StudentRepo.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-//JpaRepo extends crud operations so we have crud operations in japRepo
+//JpaRepo extends crud operations so we have crud operations in JpaRepo
 @Component
 public interface StudentRepo extends JpaRepository<Student,Integer> {
 //
-//    @Query("select s from Student as s where s.name = ?1")  jpql query user to search by properties of an object it takes a class name and its property to search
-//    List<Student> findByName (String name);
+    @Query("select s from Student as s where s.name = ?1")  // jpql query user to search by properties of an object it takes a class name and its property to search
+    List<Student> findByName (String name);
 }
